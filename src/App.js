922368cd72d16f6405d317c9711f3de7d79d3d49
unknown <saparbekov.ilzar@bk.rugit config --global user.name>
2023-03-07 15:34:55 +0600

    dz

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { fetchPokemons } from "./api/fetchPokemons";
+import PokemonCard from "./components/PokemonCard/PokemonCard";
+const App = () =>  {
+  const [ theme, setTheme ] = useState('dark');
+  const [ list, setList ] = useState([]);
+
+  const toogleTheme  = () => {
+    const newTheme = theme === 'dark' ? 'light' : 'dark'
+    setTheme(newTheme)
+  }
+
+useEffect(() => {
+  fetchPokemons(setList)
+}, [])
+console.log(list, 'list',);
+  return (
+    <div className={`app ${theme}`}>
+      App
+      <button 
+      onClick={toogleTheme}
+      className="button">
+        Change Theme
+      </button>
+        <div className='blocks'>
+            {
+                list.map((i, k) => <PokemonCard key={k} list={i}/>)
+            }
+        </div>
+    </div>
+  );
+}
+
+export default App;
